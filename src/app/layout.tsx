c8b05950a64241b2fca5f45be8514f8b33675d9c
Mehedi <mjimehedi99@gmail.com>
2024-11-05 15:05:48 +0600

    🔖 Lay out Completed
    jikfa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Sora } from "next/font/google";
+import React from "react";
+import Header from "@/components/Header";
+import TopLeftImage from "@/components/TopLeftImage";
+import Nav from "@/components/Nav";
 
-const inter = Inter({ subsets: ["latin"] });
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+      >
+        <TopLeftImage />
+        <Nav />
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/TopLeftImage.tsx
+++ src/components/TopLeftImage.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
 
 const TopLeftImage = () => {
-  return <div>TopLeftImage</div>;
+  return (
+    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
+      <Image src="/top-left-img.png" width={400} height={400} alt="" />
+    </div>
+  );
 };
 
 export default TopLeftImage;
