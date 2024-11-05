038d0404ec176a3b45d66ddc7678aed2a99ebc54
Mehedi <mjimehedi99@gmail.com>
2024-11-05 14:25:28 +0600

    🔥 Ready to Start
    Still dont know

--- src/components/WorkSlider.tsx
+++ src/components/WorkSlider.tsx
@@ -1,4 +1,49 @@
 import React from "react";
+// data
+const workSlides = {
+  slides: [
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: "title",
+          path: "/thumb4.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb1.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb2.jpg",
+        },
+        {
+          title: "title",
+          path: "/thumb3.jpg",
+        },
+      ],
+    },
+  ],
+};
 
 const WorkSlider = () => {
   return <div>WorkSlider</div>;
